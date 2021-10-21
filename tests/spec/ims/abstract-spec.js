49cb8c0d1bf9da3db9164136b6b0db242b77212c
Andy Miller (IMS) <48326098+amiller-ims@users.noreply.github.com>
2021-10-21 16:20:00 -0700

    Test for handling of missing abstract

--- tests/spec/ims/abstract-spec.js
+++ tests/spec/ims/abstract-spec.js
@@ -2,6 +2,7 @@ import { flushIframes, makeRSDoc, makeStandardImsOps } from "../SpecHelper.js";
 
 describe("IMS — Abstract", () => {
   afterAll(flushIframes);
+
   it("includes a h2 and sets the class", async () => {
     const ops = makeStandardImsOps();
     const doc = await makeRSDoc(ops);
@@ -11,5 +12,20 @@ describe("IMS — Abstract", () => {
     expect(h2.textContent).toBe("Abstract");
     expect(abs.classList).toContain("introductory");
     expect(abs.querySelector("p")).toBeTruthy();
+    const pill = doc.getElementById("respec-pill-warning");
+    expect(pill).toBeFalsy();
+  });
+
+  it("warns if abstract is missing", async () => {
+    const ops = makeStandardImsOps();
+    ops.abstract = null;
+    const doc = await makeRSDoc(ops);
+    const pill = doc.getElementById("respec-pill-warning");
+    expect(pill).toBeTruthy();
+    expect(pill.textContent).toEqual("1");
+    // ims/abstract will insert a blank, empty abstract
+    // with class "remove" so it should be missing
+    const abs = doc.getElementById("abstract");
+    expect(abs).toBeFalsy();
   });
 });
