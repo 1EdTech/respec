3f8a75cf2ab104cb92e30c667a82b19ae77e7cbc
Andy Miller (IMS) <48326098+amiller-ims@users.noreply.github.com>
2021-10-29 07:48:41 -0700

    Use one fetch

--- src/ims/cdm.js
+++ src/ims/cdm.js
@@ -9,97 +9,68 @@
  *
  * The HTML created by the CDM parser is a table for each data class.
  */
+import { showError, showWarning } from "../core/utils.js";
 import dataClassTmpl from "./templates/dataClass.js";
 import dataModelTmpl from "./templates/dataModel.js";
 import { html } from "../core/import-maps.js";
-import { showError } from "../core/utils.js";
 
 export const name = "ims/cdm";
 
 /**
- * Process a single data model class definition.
+ * Replaces the heading text and the TOC entry.
  *
- * @param {string} id The CDM id for the class.
+ * @param {HTMLHeadingElement} header The heading element to change
+ * @param {String} headerText The new text
  */
-async function processDataClass(id) {
-  const query = JSON.stringify({
-    query: `
-    {
-      classByID(id: "${id}") {
-        id
-        name
-        documentation {
-          description
-        }
-        properties {
-          name
-          type {
-            id
-            name
-          }
-          cardinality {
-            value
-          }
-          documentation {
-            description
-          }
-        }
+function replaceHeaderText(header, headerText) {
+  let textChanged = false;
+  header.childNodes.forEach(node => {
+    if (node.nodeType === 3) {
+      if (textChanged) {
+        node.remove();
+      } else {
+        node.nodeValue = headerText;
+        textChanged = true;
       }
-    }`,
+    }
   });
-
-  fetch("https://imsum2.herokuapp.com/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // eslint-disable-next-line no-undef
-      "X-Api-Key": env.API_KEY,
-    },
-    body: query,
-  })
-    .then(res => res.json())
-    .then(data => {
-      console.log("fetched data", data);
-      const payload = data.data.classByID;
-      const section = document.getElementById(payload.id);
-      const header = section.querySelector("h3");
-      if (header) {
-        // Replace the placeholder text with the class name
-        const headerText = payload.name;
-        let textChanged = false;
-        header.childNodes.forEach(node => {
-          if (node.nodeType === 3) {
-            if (textChanged) {
-              node.remove();
-            } else {
-              node.nodeValue = headerText;
-              textChanged = true;
-            }
-          }
-        });
-        // Replace the TOC placeholder text with the same name
-        const tocxref = document.querySelector(
-          `a.tocxref[href='#${header.id}']`
-        );
-        if (tocxref) {
-          let textChanged = false;
-          tocxref.childNodes.forEach(node => {
-            if (node.nodeType === 3) {
-              if (textChanged) {
-                node.remove();
-              } else {
-                node.nodeValue = headerText;
-                textChanged = true;
-              }
-            }
-          });
-        }
-        const fullElem = dataClassTmpl(payload);
-        if (fullElem) {
-          section.append(...fullElem.childNodes);
+  // Replace the TOC placeholder text with the same name
+  const tocxref = document.querySelector(`a.tocxref[href='#${header.id}']`);
+  if (tocxref) {
+    let textChanged = false;
+    tocxref.childNodes.forEach(node => {
+      if (node.nodeType === 3) {
+        if (textChanged) {
+          node.remove();
+        } else {
+          node.nodeValue = headerText;
+          textChanged = true;
         }
       }
     });
+  }
+}
+
+/**
+ * Process a single data model class definition.
+ *
+ * @param {*} dataClass The CDM class object.
+ */
+async function processDataClass(dataClass) {
+  const section = document.getElementById(dataClass.id);
+  if (!section) {
+    showError(`Missing class ${dataClass.id}`, name);
+  } else {
+    const header = section.querySelector("h3");
+    replaceHeaderText(header, dataClass.name);
+    const fullElem = dataClassTmpl(dataClass);
+    if (fullElem) {
+      let targetElem = header;
+      fullElem.childNodes.forEach(element => {
+        targetElem = targetElem.insertAdjacentElement("afterend", element);
+      });
+    }
+  }
 }
 
 /**
@@ -114,14 +85,32 @@ async function processDataModel(id) {
       modelByID(id: "${id}") {
         id
         name
-        classes {
-          id
-        }
         documentation {
           description
         }
+        classes {
+          id
+          name
+          documentation {
+            description
+          }
+          properties {
+            name
+            type {
+              name
+              id
+            }
+            cardinality {
+              value
+            }
+            documentation {
+              description
+            }
+          }
+        }
       }
-    }`,
+    }
+    `,
   });
 
   fetch("https://imsum2.herokuapp.com/graphql", {
@@ -136,96 +125,68 @@ async function processDataModel(id) {
     .then(res => res.json())
     .then(data => {
       console.log("fetched data", data);
-      const payload = data.data.modelByID;
-      const section = document.getElementById(payload.id);
+      const dataModel = data.data.modelByID;
+      if (!dataModel) {
+        showError(`Unknown CDM ${id}`, name);
+        return;
+      }
+      const section = document.getElementById(dataModel.id);
       const header = section.querySelector("h2");
-      if (header) {
-        // Replace the placeholder text with the model name
-        const headerText = `Data Model`;
-        let textChanged = false;
-        header.childNodes.forEach(node => {
-          if (node.nodeType === 3) {
-            if (textChanged) {
-              node.remove();
-            } else {
-              node.nodeValue = headerText;
-              textChanged = true;
-            }
-          }
-        });
+      replaceHeaderText(header, `${dataModel.name} Data Model`);
 
-        // Replace the TOC placeholder text with the same name
-        const tocxref = document.querySelector(
-          `a.tocxref[href='#${header.id}']`
-        );
-        if (tocxref) {
-          let textChanged = false;
-          tocxref.childNodes.forEach(node => {
-            if (node.nodeType === 3) {
-              if (textChanged) {
-                node.remove();
-              } else {
-                node.nodeValue = headerText;
-                textChanged = true;
-              }
-            }
-          });
-        }
-        const fullElem = dataModelTmpl(payload);
-        if (fullElem) {
-          header.insertAdjacentElement("afterend", fullElem);
-        }
+      const fullElem = dataModelTmpl(dataModel);
+      if (fullElem) {
+        header.insertAdjacentElement("afterend", fullElem);
       }
 
-      // Audit classes
-      payload.classes.forEach(element => {
-        const dataClass = section.querySelector(
-          `section#${element.id.replaceAll(".", "\\.")}[data-class]`
-        );
-        if (!dataClass) {
-          showError(`Missing class ${element.id}`, name);
-        }
+      dataModel.classes.forEach(dataClass => {
+        processDataClass(dataClass);
       });
     });
 }
 
 /**
- * Process all the <dataclass> elements in the document.
+ * Process all of the <section data-model> elements in the document.
  *
- * @param {*} dataClasses Array of matching <dataclass> elements.
+ * @param {HTMLElement[]} dataModelSections An array of data-model sections
  */
-
-function processDataClasses(dataClasses) {
-  // Insert a place holder for this class. It will
-  // be replaced asynchronously
-  dataClasses.forEach(dataClass => {
-    dataClass.prepend(html`<h3>${dataClass.id}</h3>`);
-  });
-
-  // Queue filling in the details
-  dataClasses.forEach(dataClass => {
-    processDataClass(dataClass.id);
-  });
-}
-
-function processDataModels(dataModels) {
-  dataModels.forEach(dataModel => {
-    dataModel.prepend(html`<h2>${dataModel.id}</h2>`);
-    const dataClasses = dataModel.querySelectorAll("section[data-class]");
-    if (dataClasses) {
-      processDataClasses(dataClasses);
+function processDataModels(dataModelSections) {
+  dataModelSections.forEach(dataModelSection => {
+    // Insert placeholder headings to be replaced when
+    // the fetch from CDM finishes.
+    dataModelSection.prepend(html`<h2>${dataModelSection.id}</h2>`);
+    const dataClassSections = dataModelSection.querySelectorAll(
+      "section[data-class]"
+    );
+    if (dataClassSections) {
+      dataClassSections.forEach(dataClassSection => {
+        dataClassSection.prepend(html`<h3>${dataClassSection.id}</h3>`);
+      });
+    } else {
+      showWarning(
+        `No <section data-class> found for data-model ${dataModelSection.id}`,
+        name
+      );
     }
   });
 
   // Queue filling in the details
-  dataModels.forEach(dataModel => {
-    processDataModel(dataModel.id);
-  });
+  try {
+    // eslint-disable-next-line no-undef
+    if (env.API_KEY) {
+      dataModelSections.forEach(dataModelSection => {
+        processDataModel(dataModelSection.id);
+      });
+    }
+  } catch {
+    showError("Cannot read from CDM (no API_KEY defined)");
+  }
 }
 
 /**
- * Convert <dataclass> elements into a normative data model
- * definition using information from the Common Data Model.
+ * Convert <section data-model> and <section data-class> elements into
+ * a normative data model definition using information from the Common
+ * Data Model.
  */
 export async function run() {
   const dataModels = document.querySelectorAll("section[data-model]");
