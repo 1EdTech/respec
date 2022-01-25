54ac21318c6fca4d05d07d14b22fed9a2dd2ce59
Andy Miller (IMS) <48326098+amiller-ims@users.noreply.github.com>
2022-01-25 15:42:16 -0800

    Add comments to templateUtils.js

--- src/ims/templates/templateUtils.js
+++ src/ims/templates/templateUtils.js
@@ -1,14 +1,30 @@
 // @ts-check
 import { html } from "../../core/import-maps.js";
 
+/**
+ * Render a CDM issue as a Respec issue.
+ * @param {String} issue A CDM issue ("q:" in a .lines file).
+ * @returns The issue wrapped in a div with class="issue".
+ */
 export function renderIssue(issue) {
   return html`<div class="issue">${issue}</div>`;
 }
 
+/**
+ * Render a CDM note as a Respec note.
+ * @param {String} note A CDM note ("n:" in a .lines file).
+ * @returns The note wrapped in a div with class="note".
+ */
 export function renderNote(note) {
   return html`<div class="note">${note}</div>`;
 }
 
+/**
+ * Render a term as HTML.
+ * @param {*} term A CDM model for a property representing an enumeration
+ * or vocabulary term.
+ * @returns A table row with information about the term.
+ */
 export function renderTerm(term) {
   return html`<tr>
     <td id="${term.name.toLowerCase()}">${term.name}</td>
