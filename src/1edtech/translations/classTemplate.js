e89d78bce6ffa67be4319f7313289f02a478cbde
Xavi Aracil <xaracil@1edtech.org>
2025-01-09 12:06:22 +0100

    Localize classTemplate to spanish and catalan

---
+++ src/1edtech/translations/classTemplate.js
@@ -0,0 +1,26 @@
+export default {
+  en: {
+    Property: "Property",
+    Type: "Type",
+    Description: "Description",
+    Multiplicity: "Multiplicity",
+    Privacy: "Privacy",
+    ClassExtensibility: "This class can be extended with additional properties."
+  },
+  es: {
+    Property: "Propiedad",
+    Type: "Tipo",
+    Description: "Descripción",
+    Multiplicity: "Cardinalidad",
+    Privacy: "Privacidad",
+    ClassExtensibility: "Esta clase se puede extender con propiedades adicionales."
+  },
+  ca: {
+    Property: "Propietat",
+    Type: "Tipus",
+    Description: "Descripció",
+    Multiplicity: "Cardinalitat",
+    Privacy: "Privacitat",
+    ClassExtensibility: "Aquesta classe es pot ampliar amb propietats adicionals."
+  }
+};
