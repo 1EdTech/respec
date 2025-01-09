7d955bfc58cdf0408df17e2c34e904a3e8cf4264
Xavi Aracil <xaracil@1edtech.org>
2025-01-09 12:07:07 +0100

    Localize to spanish and catalan

---
+++ src/1edtech/translations/jsonSchemasTemplate.js
@@ -0,0 +1,11 @@
+export default {
+  en: {
+    json_schema_name: "{0} JSON Schema",
+  },
+  es : {
+    json_schema_name: "JSON Schema {0}",
+  },
+  ca : {
+    json_schema_name: "JSON Schema {0}",
+  }
+};
