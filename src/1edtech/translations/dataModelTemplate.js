6b36780cc28ded154ba522c7cb3e3f9addd4b18a
Xavi Aracil <xaracil@1edtech.org>
2025-01-09 12:06:34 +0100

    Localize dataModelTemplate to spanish and catalan

---
+++ src/1edtech/translations/dataModelTemplate.js
@@ -0,0 +1,11 @@
+export default {
+  en: {
+    data_model_name: "{0} Data Model"
+  },
+  es: {
+    data_model_name: "Modelo de Datos {0}"
+  },
+  ca: {
+    data_model_name: "Model de Dades {0}"
+  }
+};
