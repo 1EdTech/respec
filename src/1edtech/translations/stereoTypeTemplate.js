93711b4b1df993ca4741d0da127429ee5dba1d93
Xavi Aracil <xaracil@1edtech.org>
2025-01-09 12:07:22 +0100

    Localize to spanish and catalan

---
+++ src/1edtech/translations/stereoTypeTemplate.js
@@ -0,0 +1,14 @@
+export default {
+  en: {
+    Type: "Type",
+    Description: "Description",
+  },
+  es : {
+    Type: "Tipo",
+    Description: "Descripción",
+  },
+  ca : {
+    Type: "Tipus",
+    Description: "Descripció",
+  }
+};
