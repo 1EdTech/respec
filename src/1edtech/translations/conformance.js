782635611da4882474630744f41dd11f9b6e0a25
Xavi Aracil <xaracil@1edtech.org>
2025-01-09 12:07:40 +0100

    Localize to spanish and catalan

---
+++ src/1edtech/translations/conformance.js
@@ -0,0 +1,44 @@
+export default {
+  en: {
+    informative_text_paragraph_1: "This document is an informative resource in the Document Set of the {0} specification {1}",
+    informative_text_paragraph_2: "As such, it does not include any normative requirements. Occurrences in this document of terms such as MAY, MUST, MUST NOT, SHOULD or RECOMMENDED have no impact on the conformance criteria for implementors of this specification.",
+    normative_text_paragraph_1: "As well as sections marked as non-normative, all authoring guidelines, diagrams, examples, and notes in this specification are non-normative. Everything else in this specification is normative.",
+    key_word: "key word",
+    key_words: "key words",
+    is: "is",
+    are: "are",
+    the: "The",
+    the_plural: "The",
+    keywords_paragraph: "in this document ${0} to be interpreted as described in ${1}.",
+    normative_text_implementation: "An implementation of this specification that fails to implement a MUST/REQUIRED/SHALL requirement or fails to abide by a MUST NOT/SHALL NOT prohibition is considered nonconformant. SHOULD/SHOULD NOT/RECOMMENDED statements constitute a best practice. Ignoring a best practice does not violate conformance but a decision to disregard such guidance should be carefully considered. MAY/OPTIONAL statements indicate that implementers are entirely free to choose whether or not to implement the option.",
+    normative_text_certification_constraints: "The <a href=\"#document-set\">Conformance and Certification Guide</a> for this specification may introduce greater normative constraints than those defined here for specific service or implementation categories."
+  },
+  es: {
+    informative_text_paragraph_1: "Este documento es un recurso informativo dentro del Set de Documentos de la especificación {0} {1}",
+    informative_text_paragraph_2: "Como tal, no incluye ningún requisito normativo. Las ocurrencias en este documento de términos como PUEDE, DEBE, NO DEBE, DEBERÍA o RECOMENDADO no tienen ningún impacto en los criterios de conformidad para los implementadores de esta especificación.",
+    normative_text_paragraph_1: "Así como las secciones marcadas como no normativas, todas las directrices de autoría, diagramas, ejemplos y notas en esta especificación no son normativas. Todo lo demás en esta especificación es normativo.",
+    key_word: "palabra clave",
+    key_words: "palabras clave",
+    is: "es",
+    are: "son",
+    the: "La",
+    the_plural: "Las",
+    keywords_paragraph: "en este documento ${0} interpretadas tal y como se describe en ${1}.",
+    normative_text_implementation: "Una implementación de esta especificación que no implementa un requisito de DEBE/REQUERIDO/DEBERIA o no cumple con una prohibición de DEBE/NO DEBE se considera no conforme. Las declaraciones DEBERÍA/NO DEBERÍA/RECOMENDADO constituyen una buena práctica. Ignorar una buena práctica no viola la conformidad, pero la decisión de ignorar dicha guía debe considerarse cuidadosamente. Las declaraciones MAY/OPCIONAL indican que los implementadores son completamente libres de elegir si implementar o no la opción.",
+    normative_text_certification_constraints: "La <a href=\"#document-set\">Guía de Conformidad y Certificación</a> para esta especificación puede introducir restricciones normativas mayores que las definidas aquí para categorías específicas de servicio o implementación."
+  },
+  ca: {
+    informative_text_paragraph_1: "Aquest document és un recurs informatiu dins del Set de Documents de l'especificació {1}",
+    informative_text_paragraph_2: "Com a tal, no inclou cap requisit normatiu. Les ocurrències en aquest document de termes com POT, HA DE, NO HA DE, o RECOMENAT no tenen cap impacte en els criteris de conformitat per als implementadors d'aquesta especificació.",
+    normative_text_paragraph_1: "A més de les seccions marcades com a no normatives, totes les directrius d'autoria, diagrames, exemples i notes en aquesta especificació són no normatives. Tota la resta en aquesta especificació és normativa.",
+    key_word: "paraula clau",
+    key_words: "paraules clau",
+    is: "és",
+    are: "són",
+    the: "La",
+    the_plural: "Les",
+    keywords_paragraph: "dins aquest document ${0} interpretades tal i com es descriu a ${1}.",
+    normative_text_implementation: "Una implementació d'aquesta especificació que no implementi un requisit HA DE/REQUERIT/HAURIA DE o que no compleixi amb una prohibició HA DE/NO HA DE es considera inconformant. Les declaracions HAURIA DE/NO HAURIA DE/RECOMENAT constitueixen una bona práctica. Ignorar una bona pràctica no viola la conformitat, però s'ha de tenir en compte amb cura la decisió d'ignorar aquesta orientació. Les declaracions POT/OPCIONAL indiquen que els implementadors són totalment lliures de triar si implementar o no l'opció.",
+    normative_text_certification_constraints: "La <a href=\"#document-set\">Guia de Conformitat i Certificació</a> per a aquesta especificació pot introduir restriccions normatives més grans que les definides aquí per a categories específiques de serveis o d'implementació."
+  }
+};
