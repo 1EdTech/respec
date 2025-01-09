62d3bace2c515a96df182f2220e5ad16446ca756
Xavi Aracil <xaracil@1edtech.org>
2025-01-09 12:07:28 +0100

    Localize to spanish and catalan

---
+++ src/1edtech/translations/templateUtils.js
@@ -0,0 +1,14 @@
+export default {
+  en: {
+    privacy_implication: "Privacy Implication",
+    enumeration_name: "{0} Enumeration",
+  },
+  es : {
+    privacy_implication: "Implicación de Privacidad",
+    enumeration_name: "Enumeración {0}",
+  },
+  ca : {
+    privacy_implication: "Implicació de Privacitat",
+    enumeration_name: "Enumeració {0}",
+  }
+};
