fa17577204aba80a4325ca44f69b30db6307a37a
Xavi Aracil <xaracil@1edtech.org>
2025-01-09 12:07:18 +0100

    Localize to spanish and catalan

---
+++ src/1edtech/translations/serviceModelTemplate.js
@@ -0,0 +1,11 @@
+export default {
+  en: {
+    service_model_name: "{0} Service Model",
+  },
+  es : {
+    service_model_name: "Modelo de Servicio {0}",
+  },
+  ca : {
+    service_model_name: "Model de Servei {0}",
+  }
+};
