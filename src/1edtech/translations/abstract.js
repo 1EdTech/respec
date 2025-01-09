dbfa2bf0ebd4e1e1184c26a140178d8449543e70
Xavi Aracil <xaracil@1edtech.org>
2025-01-09 12:07:34 +0100

    Localize to spanish and catalan

---
+++ src/1edtech/translations/abstract.js
@@ -0,0 +1,14 @@
+export default {
+  en: {
+    abstract: "Abstract",
+    to_be_removed: "To be removed"
+  },
+  es : {
+    abstract: "Resumen",
+    to_be_removed: "Para borrar"
+  },
+  ca : {
+    abstract: "Resume",
+    to_be_removed: "Per esborrar"
+  }
+};
